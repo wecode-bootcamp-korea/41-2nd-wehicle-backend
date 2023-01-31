caa191b9b018f6cbe1dfedec6383d4dfe3f62ec6
김승수 <admin@SeungSu.local>
2023-01-31 21:27:57 +0900

    [ADD] 전체상품조회,옵션필터링,주행거리필터링,연식필터링,페이지네이션,정렬기능 추가

--- src/utils/queryModules.js
+++ src/utils/queryModules.js
@@ -20,10 +20,6 @@ const forClause = (a, b) => {
   return result;
 };
 
-const whereClause = (params, params2) => {
-  return params ? `WHERE ${params2} = ${params} ` : "WHERE TRUE";
-};
-
 const andClause = (params, params2) => {
   return params ? `AND ${params2} = ${params} ` : "";
 };
@@ -32,4 +28,4 @@ const betweenClause = (params, params2, params3) => {
   return params2 ? `AND ${params3} BETWEEN ${params} AND ${params2} ` : "";
 };
 
-module.exports = { whereClause, betweenClause, forClause };
+module.exports = { betweenClause, forClause };
